cba958514600afb4be30b4a8e2be2c4714eed92d
Elijah Israel <mukisaelijah293@gmail.com>
2024-06-17 12:39:41 +0300

    'payments'

--- Payments/views.py
+++ Payments/views.py
@@ -2,30 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from Contributions.models import EventContribution
-import logging
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
 
 def payment_initiated(request):
     return render(request, 'benevofy/payment_initiated.html')
 
 
-
+@csrf_exempt
+@require_http_methods(['POST'])
 def webhook_receiver(request):
-    logger = logging.getLogger(__name__)
-
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            contributions = EventContribution.objects.filter(reference=data['customer_reference'])
-            if data['status'] == 'success':
-                contributions.update(status='Paid')
-            else:
-                contributions.delete()
-            return HttpResponse(status=200)
-        except Exception as e:
-            logger.error('Error in webhook_receiver: %s', str(e))
-            return HttpResponse(status=500)
+        data = json.loads(request.body)
+        contributions = EventContribution.objects.filter(reference=data['customer_reference'])
+        if data['status'] == 'success':
+            contributions.update(status='Paid')
+        else:
+            contributions.delete()
+        return HttpResponse(status=200)
     else:
-        logger.error('Webhook received with method other than POST')
         return HttpResponse(status=405)
                 
 
