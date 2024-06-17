8137056cf3a4b5efbc71d168c7f7194cf9dc3c28
Elijah Israel <mukisaelijah293@gmail.com>
2024-06-17 12:26:07 +0300

    'payments'

--- Payments/views.py
+++ Payments/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from Contributions.models import EventContribution
+import logging
 
 def payment_initiated(request):
     return render(request, 'benevofy/payment_initiated.html')
 
 
+
 def webhook_receiver(request):
+    logger = logging.getLogger(__name__)
+
     if request.method == 'POST':
-        data = json.loads(request.body)
-        contributions = EventContribution.objects.filter(reference=data['customer_reference'])
-        if status == 'success':
-            contributions.update(status='Paid')
-        else:
-            contributions.delete()
-        return HttpResponse(status=200)
+        try:
+            data = json.loads(request.body)
+            contributions = EventContribution.objects.filter(reference=data['customer_reference'])
+            if data['status'] == 'success':
+                contributions.update(status='Paid')
+            else:
+                contributions.delete()
+            return HttpResponse(status=200)
+        except Exception as e:
+            logger.error('Error in webhook_receiver: %s', str(e))
+            return HttpResponse(status=500)
     else:
+        logger.error('Webhook received with method other than POST')
         return HttpResponse(status=405)
                 
 
